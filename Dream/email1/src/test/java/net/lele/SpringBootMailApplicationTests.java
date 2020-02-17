d32f9176099c6c2657ab44c1e7b4be778bfce993
leleluv1122 <jh_o214@naver.com>
2020-02-17 23:36:03 +0900

    leleluv1122

---
+++ Dream/email1/src/test/java/net/lele/SpringBootMailApplicationTests.java
@@ -0,0 +1,22 @@
+package net.lele;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailApplicationTests {
+
+	@Autowired
+	CustomMailSender customMailSender;
+
+	@Test
+	void sendMail() throws MessagingException, IOException {
+		customMailSender.sendMail();
+	}
+
+}
