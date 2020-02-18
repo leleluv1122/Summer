9d31e922d778b8f5d2ff8fa89b82be06a172a919
leleluv1122 <jh_o214@naver.com>
2020-02-18 19:21:20 +0900

    leleluv1122

---
+++ Dream/cde/src/main/java/net/lele/CdeApplication.java
@@ -0,0 +1,13 @@
+package net.lele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdeApplication.class, args);
+	}
+
+}
